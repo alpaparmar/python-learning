e540baef536b5f6032a47132c4737a37a22ea022
alpa_parmar1720 <alpaparmar1720@gmail.com>
2025-03-03 17:36:29 +0530

    Python learning

---
+++ First.py
@@ -0,0 +1,79 @@
+print("Hello Begin Python")
+print(17)
+print(17+17)
+
+
+
+# Variable Declaration
+
+name = "Jay"
+age = 25
+price = 100.50
+
+print(name)
+print(age)
+print(price)
+
+
+# Data Types
+
+print(type(name))
+print(type(age))
+print(type(price))
+
+# Integer
+# Float
+# String
+# Boolean
+# None
+
+
+# First Ptogram for Sum of Two Numbers
+
+First = 10
+Second = 20
+
+result = First + Second
+
+print(result)
+
+
+# arithmetic operator
+
+# +, -, *, /, %, **
+
+# relational operator
+
+# <, >, <=, >=, ==, !=
+
+# Assignment Operator
+
+# =, +=, -=, *=, /=, %=, **=
+
+
+# Logical Operator
+
+# and, or, not
+
+# type conversion
+
+# int(), float(), str(), bool()
+
+
+# First Program from user Interface
+
+# FirstInput = input("Enter your name")
+# print(FirstInput)
+
+# Enter only input number
+
+FirstInput = int(input("Enter your number"))
+print(FirstInput)
+
+# Write a program to add two numbers from user interface
+
+FirstDataInput = int(input("Enter your first number"))
+SecondDataInput = int(input("Enter your second number"))
+
+result = FirstDataInput + SecondDataInput
+print(result)
